c5aca28c7e1008dd80404630e4585c814e1075c6
Tomas Hutchinson <tpshutch@hotmail.com>
2025-10-20 09:42:31 +0100

    FDE cycle

--- cpu.py
+++ cpu.py
@@ -9,6 +9,10 @@ class CPU:
     alu = None
 
     clock_rate = 0
+    stage = 0
+
+    current_instruction = None
+    current_data = None
 
     def __init__(self, max_memory=16, _clock_rate=20):
         self.memory = [0x00 for _ in range(max_memory)]
@@ -23,12 +27,67 @@ class CPU:
             rom = rom + [0x00 for _ in range(len(self.memory) - len(rom))]
         self.memory = rom
     
-    def cycle(self):
+    def fetch(self):
         self.registers.MAR = self.registers.PC
         self.registers.PC += 1
         self.registers.MDR = self.memory[self.registers.MAR]
+        # Ready for decode next tick
+        self.stage = 1
 
-        self.cu()
+    def decode(self):
+        instruction = self.registers.MDR >> 8
+        data = self.registers.MDR & 0xFF
+        self.current_instruction = instruction
+        self.current_data = data
+        # Ready for execute next tick
+        self.stage = 2
+
+    def execute(self):
+        inst_name = instructionset[self.current_instruction]
+        data = self.current_data
+
+        match inst_name:
+            case "END":
+                self.registers.PC = len(self.memory)
+            case "LDA":
+                self.registers.ACC = self.memory[data]
+            case "STA":
+                self.memory[data] = self.registers.ACC
+            case "ADD":
+                self.registers.ACC = self.alu.add(self.registers.ACC, self.memory[data])
+            case "SUB":
+                self.registers.ACC = self.alu.sub(self.registers.ACC, self.memory[data])
+            case "INP":
+                self.registers.ACC = int(input("Input: "))
+            case "OUT":
+                print(f"Output: {self.registers.ACC}")
+            case "BRP":
+                if self.registers.ACC >= 0:
+                    self.registers.PC = data
+            case "BRZ":
+                if self.registers.ACC == 0:
+                    self.registers.PC = data
+            case "BRA":
+                self.registers.PC = data
+            case "DAT":
+                pass  # No runtime op
+
+        # Loop back to next fetch stage
+        self.stage = 0
+
+
+    def cycle(self):
+        if self.registers.PC >= len(self.memory):
+            return False  # Program ended
+        
+        if self.stage == 0:
+            self.fetch()
+        elif self.stage == 1:
+            self.decode()
+        elif self.stage == 2:
+            self.execute()
+        
+        return True
     
     def cu(self):
         instruction = self.registers.MDR >> 8  
--- main.asm
+++ main.asm
@@ -1,29 +1,30 @@
-START   INP 0       
-        STA N              
-        LDA ZERO            
+START   INP 0
+        STA N
+        LDA ZERO
         STA F0
-        LDA ONE             
+        LDA ONE
         STA F1
 
-LOOP    LDA N              
-        SUB ONE             
-        STA N               
-        BRZ PROGRAM_END     
+LOOP    LDA N
+        SUB ONE
+        STA N
+        BRZ PROGRAM_END
+        LDA F1
+        ADD F0
+        STA TEMP
+        LDA F1
+        STA F0
+        LDA TEMP
+        STA F1
+        BRA LOOP
 
-        LDA F1              
-        ADD F0              
-        STA F1              
-        LDA F0              
-        STA F0              
-        BRA LOOP            
+PROGRAM_END LDA F1
+        OUT
+        END
 
-PROGRAM_END LDA F1          
-            OUT             
-            END             
-
-N       DAT 000             
-F0      DAT 000             
-F1      DAT 000             
-ZERO    DAT 000             
-ONE     DAT 001             
-TWO     DAT 002             
+N       DAT 0
+F0      DAT 0
+F1      DAT 0
+TEMP    DAT 0
+ZERO    DAT 0
+ONE     DAT 1
